71bef17af9fa87b12706050144931f50c6424f9a
WorkRauf <arauf.works@gmail.com>
2022-09-15 18:15:36 +0500

    initial commit

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
 });
 
 //Port settings
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
